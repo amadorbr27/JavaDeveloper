ba322d06dfd7dd9bfc3c2e2899a8604f08a6330a
Amador Rocha <abrj@cin.ufpe.br>
2022-10-19 17:47:33 -0300

    aula3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,8 +1,16 @@
 public class MinhaClasse {
     
 public static void main (String [] args) {
-    System.out.print ("Olá turma, seja bem vindo");
+    
+    String primeiroNome = "Amador";
+    String segundoNome = "Bueno";
 
+    String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+    System.out.println(nomeCompleto);
+}
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
 }
 
 }
